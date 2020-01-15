d2b212856d159f402093ec629482944d7bfa44a6
Jim Burton <j.burton@brighton.ac.uk>
2020-01-15 00:48:00 +0000

    tests

--- src/ci583/htable/test/TestHT.java
+++ src/ci583/htable/test/TestHT.java
@@ -19,20 +19,39 @@ public class TestHT {
 	
 	@Test
 	public void testEmpty() {
-		Hashtable<Boolean> h = new Hashtable<Boolean>(100);
+		Hashtable<Boolean> h = new Hashtable<>(10);
 		assertNull(h.get("foo"));
 	}
 	
 	@Test
-	public void testNotFound() {
-		Hashtable<Boolean> h = new Hashtable<Boolean>(100);
+	public void testFoundNotFound() {
+		Hashtable<Boolean> h = new Hashtable<>(10);
 		h.put("yes", true);
+		assertTrue(h.get("yes"));
 		assertNull(h.get("no"));
+	}
+
+	@Test
+	public void testDuplicates() {
+		Hashtable<String> h = new Hashtable<>(10);
+		h.put("a", "a");
+		h.put("b", "b");
+		h.put("a", "c");
+		h.put("b", "d");
+		assertEquals(h.get("a"), "c");
+		assertEquals(h.get("b"), "d");
+		assertEquals(h.getKeys().size(), 2);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testNullKey() {
+		Hashtable<Boolean> h = new Hashtable<>(10);
+		h.put(null, true);
 	}
 	
 	@Test
 	public void testInsert() {
-		Hashtable<Boolean> h = new Hashtable<Boolean>(1000, PROBE_TYPE.DOUBLE_HASH);
+		Hashtable<Boolean> h = new Hashtable<>(1000, PROBE_TYPE.DOUBLE_HASH);
 		for(int i=0;i<2000;i++) {
 			for(int j=2000;j>0;j--) {
 				h.put(i+":"+j, true);
@@ -48,13 +67,14 @@ public class TestHT {
 	
 	@Test
 	public void testGet() {
-		Hashtable<String> h = new Hashtable<String>(9);
+		Hashtable<String> h = new Hashtable<>(9);
+		int c =0;
 		for(int i=0;i<10;i++) {
 			for(int j=10;j>0;j--) {
 				h.put(i+":"+j, j+":"+i);
+				c++;
 			}
 		}
-		
 		for(int i=0;i<10;i++) {
 			for(int j=10;j>0;j--) {
 				assertEquals(h.get(i+":"+j), j+":"+i);
@@ -64,14 +84,14 @@ public class TestHT {
 	
 	@Test
 	public void testNull() {
-		Hashtable<Integer> h = new Hashtable<Integer>(20);
+		Hashtable<Integer> h = new Hashtable<>(20);
 		for(int i=0;i<10;i++) h.put(Integer.valueOf(i).toString(), Integer.valueOf(i));
 		assertNull(h.get(11+""));
 	}
 
 	@Test
 	public void testCapacity() {
-		Hashtable<Integer> h = new Hashtable<Integer>(20, Hashtable.PROBE_TYPE.LINEAR_PROBE);
+		Hashtable<Integer> h = new Hashtable<>(20, Hashtable.PROBE_TYPE.LINEAR_PROBE);
 		assertEquals(h.getCapacity(), 23);//23 is smallest prime > 20
 		for(int i=0;i<20;i++) h.put(Integer.valueOf(i).toString(), Integer.valueOf(i));
 		assertFalse(h.getCapacity() == 23);//should have resized
@@ -80,7 +100,7 @@ public class TestHT {
 	
 	@Test
 	public void testKeys() {
-		Hashtable<Integer> h = new Hashtable<Integer>(20, Hashtable.PROBE_TYPE.LINEAR_PROBE);
+		Hashtable<Integer> h = new Hashtable<>(20, Hashtable.PROBE_TYPE.LINEAR_PROBE);
 		h.put("bananas", 1);
 		h.put("pyjamas", 99);
 		h.put("kedgeree", 1);
